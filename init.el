43a04f2578efa93f343cde1d528b38b39e4fa400
Blaed <blaedj@gmail.com>
2013-01-02 15:46:46 -0600

    cleanup

--- init.el
+++ init.el
@@ -1,144 +1,137 @@
-
-
-
-
 ;; default load path
 (add-to-list 'load-path "/home/blaed/.emacs.d/")
-(add-to-list 'load-path "/home/blaed/.emacs.d/elpa/color-theme-twilight-0.1/")
-;;(add-to-list 'load-path "/home/blaed/.emacs.d/dired-details+.el")
+(add-to-list  'load-path "~/emacs.d/elpa/")
+(add-to-list  'load-path "~/emacs.d/elpa/color-theme-6.5.5/")
+;;(add-to-list  'load-path "~/emacs.d/elpa/ruby-mode-1.1/")
+(add-to-list 'load-path "~/.emacs.d/plugins/yasnippet")
+;;(add-to-list 'load-path "~/.emacs.d/elpa/wrap-region-0.6.0")
 
 (require 'dired-details+)
 
+
+;;TODO
+;;  -org mode research
+;;  -multiple-cursors on github
+
+
 ;;Marmalade package repo for package.el
 (require 'package)
-(add-to-list 'package-archives 
-    '("marmalade" .
-      "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+	     '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
-;; create the autosave dir if necessary, since emacs won't.
-(make-directory "~/.emacs.d/autosaves/" t)
 
-;;Python-Mode.el
-(add-to-list 'load-path "/home/blaed/.emacs.d/plugins/python-mode.el-6.0.12") 
-(setq py-install-directory "/home/blaed/.emacs.d/plugins/python-mode.el-6.0.12")
-(require 'python-mode)
+;;----------updated ruby mode----------------------------
+(autoload 'ruby-mode "ruby-mode" "Major mode for ruby files" t)
+(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
+(add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
+(setq inhibit-startup-screen t)
+;;-------------------------------------------------------
 
+;;------------inf-Ruby-----------------------------------
+(autoload 'inf-ruby "inf-ruby" "Run an inferior Ruby process" t)
+(autoload 'inf-ruby-keys "inf-ruby" "" t)
+(eval-after-load 'ruby-mode
+  '(add-hook 'ruby-mode-hook 'inf-ruby-keys))
+;;-------------------------------------------------------
 
-;;The following lines are for auto-complete-mode
-;;_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+;;---------auto-complete mode----------------------------
 ;;load the default configuration
 (require 'auto-complete-config)
 ;;make sure we can find the dictionaries
 (add-to-list 'ac-dictionary-directories "/home/blaed/.emacs.d/ac-dict")
 (ac-config-default)
-
 ;;turn on auto-complete-mode globally, turn off using M-x auto-complete-mode...
 (global-auto-complete-mode t)
 ;;set ac to start after 1 character is typed
 (setq ac-auto-start 1)
-;;end auto complete mode
-;;_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+;;set tab to display auto complete?...
+;TODO
 
-;;Wrap-region------------------------------------------
-(add-to-list 'load-path "/home/blaed/.emacs.d/elpa/wrap-region-0.6.0")
-(require 'wrap-region)
-(add-hook 'html-mode-hook 'wrap-region-mode)
 
-;;Attempt to set the color theme on startup
-(add-hook 'emacs-startup-hook 'color-theme-comidia)
-;;-----------------------------------------------------
+;;-------------------------------------------------------
 
-;;yasnippet plugin
-;;_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
-(add-to-list 'load-path "~/.emacs.d/plugins/yasnippet")
+;;-----------yasnippet plugin;;---------------------------
+
 (require 'yasnippet)
 (yas/global-mode 1)
 ;;Load the snippet files
 (yas/load-directory "~/.emacs.d/plugins/yasnippet/snippets/text-mode")
 ;;put snippets in autocomplete dropdown mode
 (add-to-list 'ac-sources 'ac-sources-yasnippet)
+;;-------------------------------------------------------
 
-;Set environment preferences
-;;*************************************************************************
-;;make C-x z undefined, sick of accidentally freezing emacs...
+;;-------------Wrap-region--------------------------------
+
+(require 'wrap-region)
+(add-hook 'html-mode-hook 'wrap-region-mode)
+;;-------------------------------------------------------
+
+;***************Set environment preferences*******************
+
+;;--GENERAL KEY REMAPPINGS-----------------------
+;;make C-x z undefined, don't freeze emacs...
 (global-unset-key "\C-z")
-
+;;alternative for M-x
 (global-set-key "\C-x\C-m" 'execute-extended-command)
+;;-----------------------------------------------------
 
-;;Set innitial buffer on opening
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t)
- '(js2-allow-keywords-as-property-names nil)
- '(js2-cleanup-whitespace t)
- '(js2-highlight-level 3)
- ;;deal with autosave and backup files
- ;; Put autosave files (ie #foo#) and backup files (ie foo~) in ~/.emacs.d/.
- '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
- '(backup-directory-alist '((".*" . "~/.emacs.d/backups/"))))
+;;deal with autosave and backup files
+;;Put autosave files (ie #foo#) and backup files (ie foo~) in ~/.emacs.d/.
+'(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
+'(backup-directory-alist '((".*" . "~/.emacs.d/backups/")))
+;; create the autosave dir if necessary, since emacs won't.
+(make-directory "~/.emacs.d/autosaves/" t)
 
+;;-----Get rid of the training wheels-----------------------
 ;;turn off toolbar on startup, to turn on type M-x tool-bar-mode
 (tool-bar-mode -1)
 ;;turn off scroll bars, to turn on: M-x scroll-bar-mode
 (set-scroll-bar-mode nil)
 ;;turn off menu bar, to turn back on: c-mouse3
 (menu-bar-mode -99)
-
 ;;make "y" or "n" sufficient for "yes" and "no"
 (fset 'yes-or-no-p 'y-or-n-p)
+;;---------------------------------------------------------
 
-;;---Set indentation to 2 spaces
+;;transient mark mode
+(setq transient-mark-mode t)
+
+;;remove unneccesary whitespace before saving a file
+(add-hook 'before-save-hook (lambda ()(delete-trailing-whitespace)))
+
+;;Set indentation to 2 spaces
 (setq standard-indent 2)
 (setq tab-width 2)
-;;(js2-mode tab-width 2)
 
-;;************************************************************************
+;;Enable "interactive-do"-mode
+(require 'ido)
+(ido-mode t)
 
-;;*******************inf-Ruby****************************************
- (autoload 'inf-ruby "inf-ruby" "Run an inferior Ruby process" t)
-    (autoload 'inf-ruby-keys "inf-ruby" "" t)
-    (eval-after-load 'ruby-mode
-      '(add-hook 'ruby-mode-hook 'inf-ruby-keys))
-;********************************************************************
+;;show paren mode
+(show-paren-mode t)
 
-;;COLORS -----------------------------------------------------------------
+;;***********************************************************
+
+
+;;*******COLORS ----------------------------------------------------0
+(require 'color-theme)
 ;; Set cursor color
-(set-cursor-color "medium spring green")
-     
+(set-cursor-color "magenta2")
 ;; Set mouse color
 (set-mouse-color "green")
-     
-;; Set foreground and background
-;(set-foreground-color "yellow")
-;(set-background-color "#130f28")
-
-(require 'color-theme)
-;(color-theme-initialize)
-;(load-file "~/.emacs.d/elpa/color-theme-twilight-0.1/color-theme-twilight.el")
-;(color-theme-twilight)
-
+;;set the color theme on startup
+(add-hook 'emacs-startup-hook 'color-theme-comidia)
 
 ;;; Set highlighting colors for isearch and drag
 (set-face-foreground 'highlight "MediumOrchid4")
 (set-face-background 'highlight "chartreuse2")
-     
-(set-face-foreground 'region "cyan")
-(set-face-background 'region "blue")
-     
+(set-face-foreground 'region "#000000")
+(set-face-background 'region "chartreuse2")
 (set-face-foreground 'secondary-selection "skyblue")
-
-;;-----------------------------------------------------------------------
-
-
+;;------------------------------------------------------------------0
 
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(js2-warning ((t (:underline "orange")))))
 (put 'narrow-to-region 'disabled t)
