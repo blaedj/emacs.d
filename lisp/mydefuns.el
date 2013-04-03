dfc4368501c4360d456705d9a9eac6d5617143aa
Blaed <blaedj@gmail.com>
2013-04-03 15:09:51 -0500

    add packages back in

---
+++ lisp/mydefuns.el
@@ -0,0 +1,15 @@
+(defun dired-back-to-top ()
+  (interactive)
+  (beginning-of-buffer)
+  (dired-next-line 4))
+
+(define-key dired-mode-map
+  (vector 'remap 'beginning-of-buffer) 'dired-back-to-top)
+
+(defun dired-jump-to-bottom ()
+  (interactive)
+  (end-of-buffer)
+  (dired-next-line -1))
+
+(define-key dired-mode-map
+  (vector 'remap 'end-of-buffer) 'dired-jump-to-bottom)
