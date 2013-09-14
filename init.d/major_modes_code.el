1e9233608aebe1bf125400c2ae9216e3718dfc2d
blaed <blaed@fiddler.(none)>
2013-09-14 13:48:08 -0500

    add coding hook to c mode

--- init.d/major_modes_code.el
+++ init.d/major_modes_code.el
@@ -17,6 +17,7 @@
 (add-hook 'html-mode-hook 'my-coding-hook)
 (add-hook 'web-mode-hook 'my-coding-hook)
 (add-hook 'c++-mode 'my-coding-hook)
+(add-hook 'c-mode-hook 'my-coding-hook)
 (add-hook 'html-mode-hook 'wrap-region-mode)
 
 (add-hook 'js2-mode-hook 'skewer-mode)
