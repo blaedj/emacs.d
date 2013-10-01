f25c8914c3d99dff834c691d198241c05616ce2e
Blaed <blaedj@gmail.com>
2013-10-01 08:37:47 -0500

    change modeline color on flycheck errors

--- init.el
+++ init.el
@@ -35,7 +35,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(flycheck-color-mode-line-error-face ((t (:inherit flycheck-fringe-error :background "orange red" :foreground "#efefef" :weight normal)))))
 
 
 (custom-set-variables
@@ -48,6 +48,7 @@
  '(background-mode dark)
  '(custom-safe-themes (quote ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "ddccd8fc03f09fb432acd73ba957df0baf764d8f3cb9045c2a77fc0fee8f54b9" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "72cc9ae08503b8e977801c6d6ec17043b55313cda34bcf0e6921f2f04cf2da56" default)))
  '(fci-rule-color "#383838")
+ '(flycheck-highlighting-mode (quote symbols))
  '(foreground-color "#839496")
  '(js2-basic-offset 2)
  '(js2-bounce-indent-p t)
