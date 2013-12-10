4da1e9d619c75efe0dd0d6419e1dfe6198f29a3c
Blaed <blaedj@gmail.com>
2013-12-09 18:16:11 -0600

    more zeal

---
+++ .cask/24.3.1/elpa/zeal-at-point-20131130.1620/zeal-at-point-autoloads.el
@@ -0,0 +1,34 @@
+;;; zeal-at-point-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (zeal-at-point-set-docset zeal-at-point) "zeal-at-point"
+;;;;;;  "zeal-at-point.el" (21146 112 496422 982000))
+;;; Generated autoloads from zeal-at-point.el
+
+(autoload 'zeal-at-point "zeal-at-point" "\
+Search for the word at point in Zeal
+
+\(fn &optional EDIT-SEARCH)" t nil)
+
+(autoload 'zeal-at-point-set-docset "zeal-at-point" "\
+Set current buffer's docset.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("zeal-at-point-pkg.el") (21146 112 529317
+;;;;;;  431000))
+
+;;;***
+
+(provide 'zeal-at-point-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; zeal-at-point-autoloads.el ends here
---
+++ .cask/24.3.1/elpa/zeal-at-point-20131130.1620/zeal-at-point-pkg.el
@@ -0,0 +1,1 @@
+(define-package "zeal-at-point" "20131130.1620" "Search the word at point with Zeal" (quote nil))
