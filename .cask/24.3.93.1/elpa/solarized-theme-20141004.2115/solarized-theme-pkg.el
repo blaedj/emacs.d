914abc71507937aff517c76718967fcdfd5552fe
Blaed <blaedj@gmail.com>
2014-10-08 11:17:56 -0500

    update packages

---
+++ .cask/24.3.93.1/elpa/solarized-theme-20141004.2115/solarized-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "solarized-theme" "20141004.2115" "The Solarized color theme, ported to Emacs." '((dash "2.6.0")))
---
+++ .cask/24.3.93.1/elpa/w3m-20141006.1656/w3m-pkg.el
@@ -0,0 +1,1 @@
+(define-package "w3m" "20141006.1656" "an Emacs interface to w3m" 'nil :keywords ("w3m" "www" "hypermedia"))
