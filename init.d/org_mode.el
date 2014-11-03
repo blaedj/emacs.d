8452f98bf2d3c6972ae09d48690570cafd9f275d
blaedj <blaedj@gmail.com>
2014-11-03 14:31:44 -0600

    wrap text in org mode at 72 chars
    enable auto-fill-mode in org-mode-hook and set the fill column to 72 characters.

--- init.d/org_mode.el
+++ init.d/org_mode.el
@@ -3,4 +3,9 @@
 (setq org-log-done t)
 
 (setq org-agenda-files (list "~/org/school.org"
-                             "~/org/home.org"))t
+                             "~/org/home.org"))
+
+(add-hook 'org-mode-hook
+	  '(lambda ()
+	     (turn-on-auto-fill)
+	     (set-fill-column 80)))
