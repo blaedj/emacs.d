b1ac120104f177263de55fac0a3e673c71969395
blaedj <blaedj@gmail.com>
2015-04-20 08:47:27 -0500

    Detect platform, set custom option and command key behavior on Mac

--- init.d/platform_specific.el
+++ init.d/platform_specific.el
@@ -1,15 +1,21 @@
 ;;emacs can't find ispell on OSX for some reason
 ;; probably because the default homebrew install location
 ;; isn't on the exec-path by default
-(if (eq system-type 'darwin)
-    (setq ispell-program-name "/usr/local/bin/ispell")
-  ;; add homebrew (/usr/local/bin) to the exec path
-  ;(setenv "PATH" (concat "/opt/local/bin:/usr/local/bin:" (getenv "PATH")))
-  (push "/opt/local/bin" exec-path)
-  ;; it's possible that the exec-path-from-shell could
-  ;; make the code above unneeded
-  (exec-path-from-shell-initialize);set the $MANPATH, $PATH and exec-path from the shell
-  )
+(if (eq system-type 'darwin) ((lambda ()
+			      (setq ispell-program-name "/usr/local/bin/ispell")
+			      ;; add homebrew (/usr/local/bin) to the exec path
+					;(setenv "PATH" (concat "/opt/local/bin:/usr/local/bin:" (getenv "PATH")))
+			      (push "/opt/local/bin" exec-path)
+			      ;; it's possible that the exec-path-from-shell could
+			      ;; make the code above unneeded
+			      (exec-path-from-shell-initialize);set the $MANPATH, $PATH and exec-path from the shell
+
+			      ;; set keys for Apple keyboard, for emacs in OS X
+			      (setq mac-command-modifier 'meta) ; make cmd key do Meta
+			      (setq mac-option-modifier 'super) ; make cmd key do Meta
+			      ;;(setq mac-command-modifier 'super) ; make cmd key do super
+			      ;;(global-unset-key (kbd "s-l") )
+			      )))
 
 ;; Xiki
 ;; Load el4r, which loads Xiki
