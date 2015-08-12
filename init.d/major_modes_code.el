412d0b76de40fe1e31903f67c26399433145210c
blaedj <blaedj@gmail.com>
2015-08-12 08:29:14 -0500

    open .ejs files (client-side js templating) with web-mode

--- init.d/major_modes_code.el
+++ init.d/major_modes_code.el
@@ -245,6 +245,7 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode) )
 (add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.ejs\\'" . web-mode))
 
 (add-to-list 'auto-mode-alist '("\\.ex\\'" . elixir-mode))
 (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode) )
