981c0c9a58b265cc554813806e2317e50f725234
blaedj <blaedj@gmail.com>
2015-12-23 09:47:25 -0800

    open a shared 'todo' file on startup

--- init.d/org_mode.el
+++ init.d/org_mode.el
@@ -14,3 +14,14 @@
 	  '(lambda ()
 	     (turn-on-auto-fill)
 	     (set-fill-column 80)))
+
+;; todo.org file, probably in dropbox if dropbox is installed
+(setq todo-org-file-name "~/Dropbox/todo.org")
+(defun open-todo ()
+  "Opens the todo.org file stored in dropbox, if presnet"
+  (interactive)
+  (if (file-exists-p todo-org-file-name)
+      (find-file todo-org-file-name)
+      ))
+
+(push #'open-todo after-init-hook)
