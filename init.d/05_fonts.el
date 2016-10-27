9bbb2a760c5c707451e93146dacc1bfce7683f1c
blaedj <blaedj@gmail.com>
2016-10-27 09:17:50 -0500

    increase font sized a little
     #youregettingold?

--- init.d/05_fonts.el
+++ init.d/05_fonts.el
@@ -1,4 +1,4 @@
-;(set-frame-font "Monaco 13") ; great font, may need to install on non-macs
+;(set-frame-font "Monaco 14") ; great font, may need to install on non-macs
 ;;(set-frame-font "Hack 14") ; will need to install. Look at http://sourcefoundry.org/hack/ for the latest downloads.
 ;; (set-frame-font "SourceCodePro 11")
 (set-frame-font "-*-Source Code Pro-light-normal-normal-*-*-*-*-*-m-0-iso10646-1")
@@ -10,4 +10,5 @@
 		    ;; :height 'normal
 		    )
 
-;; (set-frame-font "Anonymous Pro 12")
+
+;; (set-frame-font "Anonymous Pro 13")
