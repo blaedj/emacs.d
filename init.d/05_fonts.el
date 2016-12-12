15283b4f5543c419ff38c8a3015a653bce6c7a0f
blaedj <blaedj@gmail.com>
2016-12-12 09:04:54 -0600

    increase default font size.
     #gettingold

--- init.d/05_fonts.el
+++ init.d/05_fonts.el
@@ -1,10 +1,10 @@
 ;(set-frame-font "Monaco 14") ; great font, may need to install on non-macs
-;;(set-frame-font "Hack 14") ; will need to install. Look at http://sourcefoundry.org/hack/ for the latest downloads.
+;;(set-frame-font "Hack 16") ; will need to install. Look at http://sourcefoundry.org/hack/ for the latest downloads.
 ;; (set-frame-font "SourceCodePro 11")
 (set-frame-font "-*-Source Code Pro-light-normal-normal-*-*-*-*-*-m-0-iso10646-1")
 (set-face-attribute 'default nil
 		    :family "Source Code Pro"
-		    :height 130
+		    :height 135
 		    :weight 'light
 		    :width 'normal
 		    ;; :height 'normal
