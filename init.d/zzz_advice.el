bc9c58cb25143137adc344bdcd54940b8170bcb7
blaedj <blaedj@gmail.com>
2017-01-26 09:06:43 -0600

    Disable ansi-term buffer renaming

--- init.d/zzz_advice.el
+++ init.d/zzz_advice.el
@@ -4,7 +4,10 @@
 
 (require 'mydefuns )
 
-(advice-add 'ansi-term :around #'bcj-open-project-term)
+;; TODO: need to fix this to handle the case when the buffer name already
+;; exists; currently it throws an error and causes my window setup funciton to
+;; terminate early.
+;;(advice-add 'ansi-term :around #'bcj-open-project-term)
 
 (provide 'zzz_advice)
 ;;; zzz_advice.el ends here
