b172a614979de122bb51c84f5c454d301f559594
blaedj <blaedj@gmail.com>
2017-02-15 10:01:01 -0600

    ignore some files, move auto-saved customizations to a seperate file
    Also add some snippets and yasnippet setup

--- init.el
+++ init.el
@@ -15,53 +15,13 @@
 ;; No splash screen please ...
 (setq inhibit-startup-message t)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default bold shadow italic underline bold bold-italic bold])
- '(ansi-color-names-vector
-   (vector "#ffffff" "#f36c60" "#8bc34a" "#fff59d" "#4dd0e1" "#b39ddb" "#81d4fa" "#263238"))
- '(background-color "#002b36")
- '(background-mode dark)
- '(coffee-tab-width 2)
- '(cursor-color "#657b83")
- '(custom-safe-themes
-   (quote
-    ("70053ac78af15b0bdc93f4aea748b2e43e4e578b068600c3e32d468175f143bd" "790e74b900c074ac8f64fa0b610ad05bcfece9be44e8f5340d2d94c1e47538de" "e97dbbb2b1c42b8588e16523824bc0cb3a21b91eefd6502879cf5baa1fa32e10" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "f11b028f78c8934c4dea255d94c491f7ced8720db594f9454dbec55938af3934" "1c57936ffb459ad3de4f2abbc39ef29bfb109eade28405fa72734df1bc252c13" "31a01668c84d03862a970c471edbd377b2430868eccf5e8a9aec6831f1a0908d" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
- '(fci-rule-color "#37474f")
- '(hl-sexp-background-color "#efebe9")
- '(org-agenda-files nil)
- '(paradox-github-token t)
- '(safe-local-variable-values
-   (quote
-    ((eval setq org-agenda-files
-	   (list
-	    (concat org-directory "/todo.org"))))))
- '(vc-annotate-background nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#f36c60")
-     (40 . "#ff9800")
-     (60 . "#fff59d")
-     (80 . "#8bc34a")
-     (100 . "#81d4fa")
-     (120 . "#4dd0e1")
-     (140 . "#b39ddb")
-     (160 . "#f36c60")
-     (180 . "#ff9800")
-     (200 . "#fff59d")
-     (220 . "#8bc34a")
-     (240 . "#81d4fa")
-     (260 . "#4dd0e1")
-     (280 . "#b39ddb")
-     (300 . "#f36c60")
-     (320 . "#ff9800")
-     (340 . "#fff59d")
-     (360 . "#8bc34a"))))
- '(vc-annotate-very-old-color nil))
+
+(defvar bcj-emacs-init-file (or load-file-name buffer-file-name))
+
+(defvar bcj-emacs-config-dir (file-name-directory bcj-emacs-init-file))
+(defvar bcj-init-dir  (expand-file-name "init.d" bcj-emacs-config-dir ))
+
+(setq custom-file (expand-file-name "custom.el" bcj-init-dir))
 
 ;; Display the total loading time in the minibuffer
 
@@ -69,10 +29,6 @@
   "Display startup echo area message."
   (message "Initialized in %s" (emacs-init-time)))
 
-(defvar bcj-emacs-init-file (or load-file-name buffer-file-name))
-
-(defvar bcj-emacs-config-dir (file-name-directory bcj-emacs-init-file))
-(defvar bcj-init-dir  (expand-file-name "init.d" bcj-emacs-config-dir ))
 
 
 ;; set secrets
