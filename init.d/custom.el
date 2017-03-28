fa19416079dba73920039485e3615a39ad91393b
blaedj <blaedj@gmail.com>
2017-03-28 09:27:31 -0500

    updates to custom.el...

--- init.d/custom.el
+++ init.d/custom.el
@@ -3,49 +3,21 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default bold shadow italic underline bold bold-italic bold])
- '(ansi-color-names-vector
-   (vector "#ffffff" "#f36c60" "#8bc34a" "#fff59d" "#4dd0e1" "#b39ddb" "#81d4fa" "#263238"))
- '(background-color "#002b36")
- '(background-mode dark)
  '(coffee-tab-width 2)
- '(cursor-color "#657b83")
  '(custom-safe-themes
    (quote
-    ("70053ac78af15b0bdc93f4aea748b2e43e4e578b068600c3e32d468175f143bd" "790e74b900c074ac8f64fa0b610ad05bcfece9be44e8f5340d2d94c1e47538de" "e97dbbb2b1c42b8588e16523824bc0cb3a21b91eefd6502879cf5baa1fa32e10" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "f11b028f78c8934c4dea255d94c491f7ced8720db594f9454dbec55938af3934" "1c57936ffb459ad3de4f2abbc39ef29bfb109eade28405fa72734df1bc252c13" "31a01668c84d03862a970c471edbd377b2430868eccf5e8a9aec6831f1a0908d" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
- '(fci-rule-color "#37474f")
- '(hl-sexp-background-color "#efebe9")
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
  '(js-indent-level 2)
- '(org-agenda-files nil)
  '(package-selected-packages
    (quote
-    (company-quickhelp pos-tip yasnippet hydra flycheck-dogma org-gcal org-bullets tomatinho zeal-at-point yaml-mode www-synonyms wrap-region windresize web-mode w3m twilight-theme swift-mode subatomic256-theme spaces solarized-theme smex smartscan smart-region smart-mode-line slime-repl skewer-mode sicp scss-mode rvm rspec-mode robe rainbow-mode projectile project-explorer popup-complete php-mode paradox pallet names material-theme markdown-mode main-line magit keyfreq imenu-anywhere image+ ido-vertical-mode ido-ubiquitous idle-highlight highlight-indentation hide-lines haml-mode hackernews git-gutter-fringe flymake-coffee flycheck-elixir flycheck-color-mode-line flycheck-cask flx-ido floobits find-file-in-project fill-column-indicator figlet exec-path-from-shell evil ereader enh-ruby-mode emojify dash-at-point csv-mode cpputils-cmake column-enforce-mode color-theme-solarized color-theme-heroku color-theme-approximate coffee-mode cmake-mode buffer-move bubbleberry-theme auto-org-md auto-complete-clang alchemist ag adaptive-wrap ace-window ace-jump-mode ac-etags ac-cider 2048-game)))
- '(paradox-github-token t)
- '(safe-local-variable-values
-   (quote
-    ((eval setq org-agenda-files
-	   (list
-	    (concat org-directory "/todo.org"))))))
- '(vc-annotate-background nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#f36c60")
-     (40 . "#ff9800")
-     (60 . "#fff59d")
-     (80 . "#8bc34a")
-     (100 . "#81d4fa")
-     (120 . "#4dd0e1")
-     (140 . "#b39ddb")
-     (160 . "#f36c60")
-     (180 . "#ff9800")
-     (200 . "#fff59d")
-     (220 . "#8bc34a")
-     (240 . "#81d4fa")
-     (260 . "#4dd0e1")
-     (280 . "#b39ddb")
-     (300 . "#f36c60")
-     (320 . "#ff9800")
-     (340 . "#fff59d")
-     (360 . "#8bc34a"))))
- '(vc-annotate-very-old-color nil))
+    (restclient el2org smartparens vue-mode handlebars-mode dot-mode counsel yasnippet zeal-at-point yaml-mode www-synonyms wrap-region windresize web-mode w3m twilight-theme tomatinho swift-mode subatomic256-theme spaces solarized-theme smex smartscan smart-region smart-mode-line slime skewer-mode sicp scss-mode rvm rspec-mode robe rainbow-mode projectile project-explorer popup-complete php-mode paradox pallet org-gcal org-bullets names material-theme markdown-mode main-line magit keyfreq imenu-anywhere image+ ido-vertical-mode ido-ubiquitous highlight-indentation hide-lines haml-mode hackernews git-gutter-fringe flymake-coffee flycheck-elixir flycheck-color-mode-line flycheck-cask flx-ido floobits find-file-in-project fill-column-indicator figlet exec-path-from-shell evil ereader enh-ruby-mode emojify dumb-jump dash-at-point csv-mode cpputils-cmake company-quickhelp column-enforce-mode color-theme-solarized color-theme-approximate coffee-mode cmake-mode buffer-move bubbleberry-theme auto-org-md auto-complete-clang alchemist ag adaptive-wrap ace-window ace-jump-mode ac-etags ac-cider 2048-game)))
+ '(paradox-automatically-star t)
+ '(paradox-github-token t t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(flycheck-color-mode-line-error-face ((t (:inherit flycheck-fringe-error :background "orange red" :foreground "#efefef" :weight normal))))
+ '(hl-line ((t (:inverse-video t :underline nil))))
+ '(magit-diff-added ((t (:inherit diff-added)))))
